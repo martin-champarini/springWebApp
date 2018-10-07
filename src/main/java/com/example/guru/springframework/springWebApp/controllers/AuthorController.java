55b4e81dda45fa0382e9ae7ff29e72a301ad4e6f
Martin Champarini <martin.champarini.dev@gmail.com>
2018-10-07 09:52:43 -0300

    Added Thymeleaf and AuthorController s2-24

---
+++ src/main/java/com/example/guru/springframework/springWebApp/controllers/AuthorController.java
@@ -0,0 +1,23 @@
+package com.example.guru.springframework.springWebApp.controllers;
+
+import com.example.guru.springframework.springWebApp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+
+}
